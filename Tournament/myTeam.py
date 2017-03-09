a150c03605949a29d4f9018a06770334a5c209bd
Ryan Monroe <rmonroe@ucsc.edu>
2017-03-09 00:19:46 -0800

    base reflex agent

--- Tournament/myTeam.py
+++ Tournament/myTeam.py
@@ -7,6 +7,7 @@
 # For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
 
 from captureAgents import CaptureAgent
+from baselineTeam import ReflexCaptureAgent
 import random, time, util
 from game import Directions
 import game
@@ -31,13 +32,72 @@ def createTeam(firstIndex, secondIndex, isRed,
   any extra arguments, so you should make sure that the default
   behavior is what you want for the nightly contest.
   """
-
+  first = 'SanteOffense'
+  second = 'SanteDefense'
   # The following line is an example only; feel free to change it.
   return [eval(first)(firstIndex), eval(second)(secondIndex)]
 
 ##########
 # Agents #
 ##########
+class SanteOffense(ReflexCaptureAgent):
+    """
+    A reflex agent that seeks food. This is an agent
+    we give you to get an idea of what an offensive agent might look like,
+    but it is by no means the best or only way to build an offensive agent.
+    """
+    def getFeatures(self, gameState, action):
+      features = util.Counter()
+      successor = self.getSuccessor(gameState, action)
+      features['successorScore'] = self.getScore(successor)
+
+      # Compute distance to the nearest food
+      foodList = self.getFood(successor).asList()
+      if len(foodList) > 0: # This should always be True,  but better safe than sorry
+        myPos = successor.getAgentState(self.index).getPosition()
+        minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
+        features['distanceToFood'] = minDistance
+      return features
+
+    def getWeights(self, gameState, action):
+      return {'successorScore': 100, 'distanceToFood': -1}
+class SanteDefense(ReflexCaptureAgent):
+    """
+    A reflex agent that keeps its side Pacman-free. Again,
+    this is to give you an idea of what a defensive agent
+    could be like.  It is not the best or only way to make
+    such an agent.
+    """
+
+    def getFeatures(self, gameState, action):
+      features = util.Counter()
+      successor = self.getSuccessor(gameState, action)
+
+      myState = successor.getAgentState(self.index)
+      myPos = myState.getPosition()
+
+      # Computes whether we're on defense (1) or offense (0)
+      features['onDefense'] = 1
+      if myState.isPacman: features['onDefense'] = 0
+
+      # Computes distance to invaders we can see
+      enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
+      invaders = [a for a in enemies if a.isPacman and a.getPosition() != None]
+      features['numInvaders'] = len(invaders)
+      if len(invaders) > 0:
+        dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
+        features['invaderDistance'] = min(dists)
+
+      if action == Directions.STOP: features['stop'] = 1
+      rev = Directions.REVERSE[gameState.getAgentState(self.index).configuration.direction]
+      if action == rev: features['reverse'] = 1
+
+      return features
+
+    def getWeights(self, gameState, action):
+      return {'numInvaders': -1000, 'onDefense': 100, 'invaderDistance': -10, 'stop': -100, 'reverse': -2}
+
+
 
 class DummyAgent(CaptureAgent):
   """
@@ -50,8 +110,8 @@ class DummyAgent(CaptureAgent):
     """
     This method handles the initial setup of the
     agent to populate useful fields (such as what team
-    we're on). 
-    
+    we're on).
+
     A distanceCalculator instance caches the maze distances
     between each pair of positions, so your agents can use:
     self.distancer.getDistance(p1, p2)
@@ -59,15 +119,15 @@ class DummyAgent(CaptureAgent):
     IMPORTANT: This method may run for at most 15 seconds.
     """
 
-    ''' 
+    '''
     Make sure you do not delete the following line. If you would like to
     use Manhattan distances instead of maze distances in order to save
     on initialization time, please take a look at
-    CaptureAgent.registerInitialState in captureAgents.py. 
+    CaptureAgent.registerInitialState in captureAgents.py.
     '''
     CaptureAgent.registerInitialState(self, gameState)
 
-    ''' 
+    '''
     Your initialization code goes here, if you need any.
     '''
 
@@ -78,9 +138,8 @@ class DummyAgent(CaptureAgent):
     """
     actions = gameState.getLegalActions(self.index)
 
-    ''' 
+    '''
     You should change this in your own agent.
     '''
 
     return random.choice(actions)
-
