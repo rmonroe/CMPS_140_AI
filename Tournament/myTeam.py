0df0284a9ea2949e1ee7a29b4c8658e0375b807c
Ryan Monroe <rmonroe@ucsc.edu>
2017-03-09 11:39:27 -0800

    add reflex agent to myTeam

--- Tournament/myTeam.py
+++ Tournament/myTeam.py
@@ -7,10 +7,11 @@
 # For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
 
 from captureAgents import CaptureAgent
-from baselineTeam import ReflexCaptureAgent
+import distanceCalculator
 import random, time, util
 from game import Directions
 import game
+from util import nearestPoint
 
 #################
 # Team creation #
@@ -32,14 +33,179 @@ def createTeam(firstIndex, secondIndex, isRed,
   any extra arguments, so you should make sure that the default
   behavior is what you want for the nightly contest.
   """
-  first = 'SanteBottom'
-  second = 'SanteTop'
+  first = 'BlitzSmartBottom'
+  second = 'BlitzSmartTop'
   # The following line is an example only; feel free to change it.
   return [eval(first)(firstIndex), eval(second)(secondIndex)]
 
 ##########
 # Agents #
 ##########
+
+class ReflexCaptureAgent(CaptureAgent):
+  """
+  A base class for reflex agents that chooses score-maximizing actions
+  """
+  def chooseAction(self, gameState):
+    """
+    Picks among the actions with the highest Q(s,a).
+    """
+    actions = gameState.getLegalActions(self.index)
+
+    # You can profile your evaluation time by uncommenting these lines
+    # start = time.time()
+    values = [self.evaluate(gameState, a) for a in actions]
+    # print 'eval time for agent %d: %.4f' % (self.index, time.time() - start)
+
+    maxValue = max(values)
+    bestActions = [a for a, v in zip(actions, values) if v == maxValue]
+
+    return random.choice(bestActions)
+
+  def getSuccessor(self, gameState, action):
+    """
+    Finds the next successor which is a grid position (location tuple).
+    """
+    successor = gameState.generateSuccessor(self.index, action)
+    pos = successor.getAgentState(self.index).getPosition()
+    if pos != nearestPoint(pos):
+      # Only half a grid position was covered
+      return successor.generateSuccessor(self.index, action)
+    else:
+      return successor
+
+  def evaluate(self, gameState, action):
+    """
+    Computes a linear combination of features and feature weights
+    """
+    features = self.getFeatures(gameState, action)
+    weights = self.getWeights(gameState, action)
+    return features * weights
+
+  def getFeatures(self, gameState, action):
+    """
+    Returns a counter of features for the state
+    """
+    features = util.Counter()
+    successor = self.getSuccessor(gameState, action)
+    features['successorScore'] = self.getScore(successor)
+    return features
+
+  def getWeights(self, gameState, action):
+    """
+    Normally, weights do not depend on the gamestate.  They can be either
+    a counter or a dictionary.
+    """
+    return {'successorScore': 1.0}
+
+#################
+# Blitzkrieg 2.0#
+#################
+class BlitzSmartBottom(ReflexCaptureAgent):
+    """
+    A reflex agent that seeks food. This is an agent
+    we give you to get an idea of what an offensive agent might look like,
+    but it is by no means the best or only way to build an offensive agent.
+    """
+    def __init__( self, index, timeForComputing = .1 ):
+        ReflexCaptureAgent.__init__( self, index, timeForComputing = .1 )
+
+    def getFeatures(self, gameState, action):
+      features = util.Counter()
+      successor = self.getSuccessor(gameState, action)
+      features['successorScore'] = self.getScore(successor)
+      foodList = self.getFood(successor).asList()
+      half = len(foodList) / 2
+      foodList = foodList[:half]
+      if len(foodList) > 0: # This should always be True,  but better safe than sorry
+        myPos = successor.getAgentState(self.index).getPosition()
+        minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
+        features['distanceToFood'] = minDistance
+
+      # Gets a list of the opponents that are currently a ghost
+      ghost= []
+      for i in self.getOpponents(gameState):
+          if not gameState.getAgentState(i).isPacman:
+              ghost.append(i)
+
+
+
+
+      return features
+
+      """
+      features['successorScore'] = self.getScore(successor)
+
+      # Compute distance to the nearest food
+
+      """
+
+
+
+    def getWeights(self, gameState, action):
+      return {'successorScore': 100, 'distanceToFood': -1}
+class BlitzSmartTop(ReflexCaptureAgent):
+    """
+    A reflex agent that seeks food. This is an agent
+    we give you to get an idea of what an offensive agent might look like,
+    but it is by no means the best or only way to build an offensive agent.
+    """
+    def getFeatures(self, gameState, action):
+      features = util.Counter()
+      successor = self.getSuccessor(gameState, action)
+      features['successorScore'] = self.getScore(successor)
+
+      # Compute distance to the nearest food
+      foodList = self.getFood(successor).asList()
+      half = len(foodList) / 2
+      foodList = foodList[half:]
+      if len(foodList) > 0: # This should always be True,  but better safe than sorry
+        myPos = successor.getAgentState(self.index).getPosition()
+        minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
+        features['distanceToFood'] = minDistance
+      return features
+
+    def getWeights(self, gameState, action):
+      return {'successorScore': 100, 'distanceToFood': -1}
+
+
+class SanteDefense(ReflexCaptureAgent):
+    """
+    A reflex agent that keeps its side Pacman-free. Again,
+    this is to give you an idea of what a defensive agent
+    could be like.  It is not the best or only way to make
+    such an agent.
+    """
+
+    def getFeatures(self, gameState, action):
+      features = util.Counter()
+      successor = self.getSuccessor(gameState, action)
+
+      myState = successor.getAgentState(self.index)
+      myPos = myState.getPosition()
+
+      # Computes whether we're on defense (1) or offense (0)
+      features['onDefense'] = 1
+      if myState.isPacman: features['onDefense'] = 0
+
+      # Computes distance to invaders we can see
+      enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
+      invaders = [a for a in enemies if a.isPacman and a.getPosition() != None]
+      features['numInvaders'] = len(invaders)
+      if len(invaders) > 0:
+        dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
+        features['invaderDistance'] = min(dists)
+
+      if action == Directions.STOP: features['stop'] = 1
+      rev = Directions.REVERSE[gameState.getAgentState(self.index).configuration.direction]
+      if action == rev: features['reverse'] = 1
+
+      return features
+
+    def getWeights(self, gameState, action):
+      return {'numInvaders': -1000, 'onDefense': 100, 'invaderDistance': -10, 'stop': -100, 'reverse': -2}
+
+
 
 ##############
 # Blitzkrieg #
@@ -50,8 +216,10 @@ the opponent. This is done, but blitzing the opponent and sending both agents
 on offense. But rather than have both agents go for the same food the "bottom"
 agent is tasked with the first set of the food list and vis versa for the top.
 Double offense seems logical given the time/move limits
+
+Win Rate vs. Baseline: ~70%
 """
-class SanteBottom(ReflexCaptureAgent):
+class BlitzBottom(ReflexCaptureAgent):
     """
     A reflex agent that seeks food. This is an agent
     we give you to get an idea of what an offensive agent might look like,
@@ -74,7 +242,7 @@ class SanteBottom(ReflexCaptureAgent):
 
     def getWeights(self, gameState, action):
       return {'successorScore': 100, 'distanceToFood': -1}
-class SanteTop(ReflexCaptureAgent):
+class BlitzTop(ReflexCaptureAgent):
     """
     A reflex agent that seeks food. This is an agent
     we give you to get an idea of what an offensive agent might look like,
